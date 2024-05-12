3d1277ca8bc30c30340399b51093f670dcb020c4
JaluDwija37 <jaludwija37@gmail.com>
2024-05-12 11:33:27 +0700

    refactor : change logo to nickname

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa";
@@ -8,9 +7,9 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className=" mx-2 w-10" src={logo} alt="logo" />
+        <h2 className="mx-2 w-10 font-bold text-2xl">JaluDwija37</h2>
       </div>
-      <div className=" m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaGithub />
         <FaTwitterSquare />
