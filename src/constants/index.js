86a9ae58c5662194569510d8eb242918b7a20c24
JaluDwija37 <jaludwija37@gmail.com>
2024-05-12 13:22:39 +0700

    refactor : use my real data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,40 +1,31 @@
 import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Enthusiastic student from the Institute of Technology and Business Malang, majoring in Informatics Engineering and specializing in Intelligent Systems. Proficient in website development and adept in artificial intelligence systems. Eager to enhance skills as a full-stack developer and delve into project management.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `As a sixth-semester student in the Department of Informatics Engineering at the Institut Teknologi dan Bisnis Asia Malang, he has not only shown a special interest in information technology, but has also developed impressive skills as a full stack developer. With expertise in the use of Django and Laravel frameworks, he is able to build complex, high-performance web applications with remarkable speed. His ability to seamlessly integrate the front-end and back-end not only reflects his expertise in coding, but also shows a deep understanding of the overall system architecture.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2023",
+    role: "Internship Full Stack Developer",
+    company: "Indi Technologi",
+    jobdesk: [
+      "Managed website maintenance and bug fixes utilizing the Laravel framework.",
+      "Spearheaded the development of a Saas website catering to UMKM (Small and Medium Enterprises).",
+      "Guided intern students from local high schools in the creation of UMKM websites, fostering collaboration and skill development.",
+    ],
+    technologies: ["Laravel", "PHP", "MySQL", "Javascript"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "2020",
+    role: "Internship Human Resource Division",
+    company: "PT Retgoo Sentris Informa",
+    jobdesk: [
+      "Provide material about PostgreSQL to prospective internship students as the minimum criteria for interns who will enter the company.",
+      "Documenting activities in the office as a report to superiors that activities are carried out well.",
+      "Performing network routing from the 2nd floor to the 4th floor so that other interns get internet access",
+    ],
+    technologies: ["PostgreSQL"],
   },
 ];
 
@@ -46,31 +37,10 @@ export const PROJECTS = [
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  // address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+62 851-5756-5885",
+  email: "jaludwija37@gmail.com",
 };
